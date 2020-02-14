a8637eda33b2b85a0a5dc981841f627b8de88337
songyi <songyi753@hotmail.com>
2020-02-14 12:54:07 +0800

    updated

--- index.php
+++ index.php
@@ -82,24 +82,6 @@ echo $actual_time;
 
 $rand =rand(1,6);//random number 1 to 6
 
-/*$conn = new mysqli('127.0.0.1', 'root','','hello');// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-$sql = "INSERT INTO songyi(secondname)
-VALUES ('John')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-*/
-
 die('error'); //exit();
 
 ?>
